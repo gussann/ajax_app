0f7ca23c424edf771c1f118ca074636fcff794de
gussann <84413122+gussann@users.noreply.github.com>
2021-06-09 20:57:15 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,15 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.order(id: "DESC")
   end
 
-  def new
-  end
+  # コメントアウトする(実装準備)
+  # def new
+  # end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index # 追記する(実装準備)
   end
 end
